d0b1688bea0e03fce4a9dc63630735d39f5842e8
JaeHongDev <println@kakao.com>
2022-03-28 09:29:59 +0900

    #3.1 Apollo Server

---
+++ server.js
@@ -0,0 +1,20 @@
+const {ApolloServer, gql, makeExecutableSchema} = require("apollo-server");
+
+const typeDefs = gql`
+    type Query{
+        hello:String
+    }
+`;
+
+
+const resolvers = {
+    Query: {
+        hello: () => "ssss",
+    }
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(() => console.log("Server is running on http://localhost:4000"));
